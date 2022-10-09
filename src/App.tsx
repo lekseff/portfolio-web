a9d338dff0397c4adc7681cd6833d130e42610f9
Aleksey Zayachnikov <lekseff@gmail.com>
2022-10-09 23:43:28 +0300

    add template

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import './App.scss';
 
 function App() {
   return (
-    <h1>Работает</h1>
+    <div className="container">
+      <header className="header">
+        <h1 className="header__title">Портфолио</h1>        
+      </header>
+      <div className="filters">
+        <button className="filters__button">Все</button>
+        <button className="filters__button active">React</button>
+        <button className="filters__button">JavaScript</button>
+      </div>
+      <main className="content">
+        <div className="content__cards cards">
+          <div className="card">
+            <div className="card__image">
+            </div>
+            <h2 className="card__title">Название проекта</h2>
+            <p className="card__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, corrupti excepturi cum unde soluta voluptas debitis facilis odio mollitia rerum minus illo quia doloribus! At accusantium facere eaque quis enim sapiente aliquid tempore voluptate dolores rem nam, sit repudiandae esse vero tempora dolorum doloremque totam iusto atque odit officia. Exercitationem.
+            </p>
+            <div className="card__control">
+              <a className="card__button-open" href="#">Открыть</a>
+              <a className="card__button-github" href="#">Посмотреть на GitHub</a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
 
